fffe6e2ce979e111c90e99e3083bd366f80e7113
JorickPepin <jorick.pepin@gmail.com>
2021-09-17 12:40:39 +0200

    ensure that the year of study is an int

--- src/constant.py
+++ src/constant.py
@@ -3,12 +3,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# USER
 LOGIN = os.getenv('LOGIN')
 PASSWORD = os.getenv('PASSWORD')
-
 YEAR_OF_STUDY = os.getenv('YEAR')
 
-# LINKS
-INSA_URL = "https://login.insa-lyon.fr/cas/login"
+if not YEAR_OF_STUDY.isdigit():
+    raise TypeError("The desired year of study (YEAR parameter) must be an integer.")
+
 PLANNING_URL = f"https://servif-cocktail.insa-lyon.fr/EdT/{YEAR_OF_STUDY}IF.php"
+INSA_URL = "https://login.insa-lyon.fr/cas/login"
